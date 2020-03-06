96f4f9dd37880c8bb33c773ed5d1eb0b77bb983f
vipinchauhan415 <61832306+vipinchauhan415@users.noreply.github.com>
2020-03-06 17:12:37 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { AuthenticationService } from './services/authentication.service';
+import { User } from './models/user';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+        selector: 'app-root',
+       templateUrl: './app.component.html',
+       styleUrls: ['./app.component.css'] })
+
+export class AppComponent {
+    currentUser: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+}
